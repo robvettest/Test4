7835355914a4294862b99a82eaceca1cf0904adb
Rob Vettor <robvet@microsoft.com>
2018-12-16 22:53:14 -0600

    Added Second App

---
+++ MyTestApp2/MyTestApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTestApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
